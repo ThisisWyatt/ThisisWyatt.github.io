e58c413fc1549affa2f59e7b78aeafbdb6e1ef06
wangy2030 <wangy2030@chinaunicom.cn>
2025-09-24 15:14:07 +0800

    Site updated: 2025-09-24 15:14:06

--- js/albums.js
+++ js/albums.js
@@ -12,8 +12,8 @@ $("img.lazy").lazyload({
 });
 
 
-if ($.fancybox) {
-  $('[data-fancybox]').fancybox({
-    protect: true
-  });
-}
+// if ($.fancybox) {
+//   $('[data-fancybox]').fancybox({
+//     protect: true
+//   });
+// }
