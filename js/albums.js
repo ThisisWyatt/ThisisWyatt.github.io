2f17d4255e6fc6006ea52bf317ee8870d2fe3af5
wangy2030 <wangy2030@chinaunicom.cn>
2025-09-24 15:47:01 +0800

    Site updated: 2025-09-24 15:46:59

--- js/albums.js
+++ js/albums.js
@@ -2,19 +2,17 @@
 $('#gallery').justifiedGallery({
   rowHeight: 150,
   margins: 3,
-  cssAnimation: true,  // 启用CSS动画
-  waitThumbnailsLoad: false,  // 不等待缩略图加载完成
 });
 
-// lazyload
-$("img.lazy").lazyload({
-  effect: "fadeIn"
-});
+// // lazyload
+// $("img.lazy").lazyload({
+//   effect: "fadeIn"
+// });
 
 
-if ($.fancybox) {
-  $('[data-fancybox]').fancybox({
-    protect: true,
-    preload: 1
-  });
-}
+// if ($.fancybox) {
+//   $('[data-fancybox]').fancybox({
+//     protect: true,
+//     preload: 1
+//   });
+// }
