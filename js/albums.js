53932e75b8bc3dda9afb833dcafd435f4d01405c
wangy2030 <wangy2030@chinaunicom.cn>
2025-09-24 15:08:14 +0800

    Site updated: 2025-09-24 15:08:12

--- js/albums.js
+++ js/albums.js
@@ -1,6 +1,6 @@
 // justifiedGallery
 $('#gallery').justifiedGallery({
-  rowHeight: 136,
+  rowHeight: 150,
   margins: 3,
   cssAnimation: true,  // 启用CSS动画
   waitThumbnailsLoad: false,  // 不等待缩略图加载完成
